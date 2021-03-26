74162110e50ee4e7270ae0d9321d2186ea72fdac
Simona Guida <80325927+simonaguida@users.noreply.github.com>
2021-03-26 10:46:08 +0100

    Update to general_result.json

--- generalcovid/grafici/OK_heatmap.py
+++ generalcovid/grafici/OK_heatmap.py
@@ -6,7 +6,7 @@ import datetime
 from dateutil.parser import parse
 
 data = []
-with open('fakecovid_result_translated_full.json', 'r') as f:
+with open('general_result.json', 'r') as f:
     for line in f:
         data.append(json.loads(line))
 
