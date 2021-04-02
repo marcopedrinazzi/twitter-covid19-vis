5c97e2b9db4b54014b9771299750aaf78efa6e21
Marco Pedrinazzi <marcopedrinazzi.mp@gmail.com>
2021-04-02 08:36:30 +0200

    table.py migliorato

--- table.py
+++ table.py
@@ -10,22 +10,35 @@ import dash
 # dash href for link in cells
 
 data = []
-with open('dataset/fakecovid_result_final_translated_full.json', 'r') as f:
+with open('fakecovid_result_final_translated_full.json', 'r') as f:
     for line in f:
         data.append(json.loads(line))
 
 index= 0
 
-with open('table.csv', 'w', newline='', encoding="utf-8") as file:
-        writer = csv.writer(file)
-        writer.writerow(["Tweet", "Link"])
-        for element in data:
-            writer.writerow([data[index]['full_text'],"http://twitter.com/anyuser/status/"+data[index]['id_str']])
-            index= index+1
+#with open('table.csv', 'w', newline='', encoding="utf-8") as file:
+#        writer = csv.writer(file)
+#        writer.writerow(["Tweet", "Link"])
+#        for element in data:
+#            writer.writerow([data[index]['full_text'],"http://twitter.com/anyuser/status/"+data[index]['id_str']])
+#            index= index+1
 
-print('FINITO')
+#print('FINITO')
 
-df = pd.read_csv('table.csv')
+txt = []
+id = []
+for element in data:
+    txt.append(data[index]['full_text'])
+    id.append("http://twitter.com/anyuser/status/"+data[index]['id_str'])
+    index=index+1
+
+df = pd.DataFrame(
+    {'Tweet': txt,
+     'Link': id
+    })
+
+print(df)
+#df = pd.read_csv('table.csv')
 
 fig = go.Figure(data=[go.Table(
     header=dict(values=list(df.columns),
