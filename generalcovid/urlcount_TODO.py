cf36148b39dc006f55106693b1312060ec104ec4
Marco Pedrinazzi <marcopedrinazzi.mp@gmail.com>
2021-04-09 12:27:37 +0200

    Update urlcount_TODO.py

--- generalcovid/urlcount_TODO.py
+++ generalcovid/urlcount_TODO.py
@@ -20,13 +20,8 @@ from dateutil.parser import parse
 import csv
 import itertools
 
-csv_dataframe = pd.read_csv('dataset/FINAL_fakecovid_final_filtered_dataset_clean.csv',sep=";")
-csv_dataframe['tweet_id'] = csv_dataframe['tweet_id'].astype(str)
-csv_list = csv_dataframe.values.tolist()
-lista_unica_csv=list(itertools.chain.from_iterable(csv_list))
-
 data = []
-with open('dataset/fakecovid_result_final_translated_full.json', 'r') as f:
+with open('dataset/general_result_translated_full.json', 'r') as f:
     for line in f:
         data.append(json.loads(line))
 
@@ -34,34 +29,22 @@ index=0
 urls = []
 titles = []
 dates = []
-category = []
 for element in data:
     print(index)
     if data[index]['entities']['urls'] is not None:
         for entity in data[index]['entities']['urls']:
-             #TIMEOUT
-            #https://stackoverflow.com/questions/16511337/correct-way-to-try-except-using-python-requests-module
-
-            # SERVONO DAVVERO SOLO QUESTI CAMPI?
             if entity['expanded_url'].lower() not in urls:
-                token_id = data[index]['id_str']                          
-                indice_csv = lista_unica_csv.index(token_id)   
-                value_cat =  lista_unica_csv[indice_csv+1].lower()
-                if value_cat == "false":
-                    value_cat = "fake"
                 try:
                     r = requests.get(entity['expanded_url'], timeout=10)
                 except requests.exceptions.Timeout:
                     titles.append("[TIMEOUT ERROR]"+"("+entity['expanded_url'].lower()+")")
                     urls.append(entity['expanded_url'].lower())
-                    category.append(value_cat.replace(" ", ""))
                     d = parse(data[index]['created_at'])
                     d = d.strftime('%Y/%m/%d')
                     dates.append(d)
                 except requests.ConnectionError:
                     titles.append("[CONNECTION ERROR]"+"("+entity['expanded_url'].lower()+")")
                     urls.append(entity['expanded_url'].lower())
-                    category.append(value_cat.replace(" ", ""))
                     d = parse(data[index]['created_at'])
                     d = d.strftime('%Y/%m/%d')
                     dates.append(d)
@@ -70,14 +53,12 @@ for element in data:
                     if soup.title is None:
                         titles.append("[NO TITLE ERROR]"+"("+entity['expanded_url'].lower()+")")
                         urls.append(entity['expanded_url'].lower())
-                        category.append(value_cat.replace(" ", ""))
                         d = parse(data[index]['created_at'])
                         d = d.strftime('%Y/%m/%d')
                         dates.append(d)
                     else:
                         titles.append("["+soup.title.text+"]"+"("+entity['expanded_url'].lower()+")")
                         urls.append(entity['expanded_url'].lower())
-                        category.append(value_cat.replace(" ", ""))
                         d = parse(data[index]['created_at'])
                         d = d.strftime('%Y/%m/%d')
                         dates.append(d)
@@ -87,9 +68,8 @@ for element in data:
 
 
 df = pd.DataFrame(
-    {'Type': category,
-    'Link': titles,
+    {'Link': titles,
     'First-Shared': dates
     })
 
-df.to_csv('urls.csv', sep=',', index=False)
+df.to_csv('urls_g.csv', sep=',', index=False)
