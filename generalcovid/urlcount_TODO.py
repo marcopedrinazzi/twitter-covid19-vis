f009cd75a2107081e5f3a750169801fdb32d3ddb
Marco Pedrinazzi <marcopedrinazzi.mp@gmail.com>
2021-04-19 17:41:36 +0200

    Update urlcount_TODO.py

--- generalcovid/urlcount_TODO.py
+++ generalcovid/urlcount_TODO.py
@@ -34,23 +34,11 @@ for element in data:
     if data[index]['entities']['urls'] is not None:
         for entity in data[index]['entities']['urls']:
             print(entity['expanded_url'])
-            if entity['expanded_url'].lower() not in urls  and (entity['expanded_url'].lower() != "https://str.sg/Jpem" or entity['expanded_url'].lower() != "https://lexington.wickedlocal.com/news/20200128/lexington-man-harvard-professor-arrested-for-lying-about-ties-to-china"):
+            if entity['expanded_url'].lower() not in urls  and entity['expanded_url'].lower() != "https://str.sg/Jpem" and entity['expanded_url'].lower() != "https://lexington.wickedlocal.com/news/20200128/lexington-man-harvard-professor-arrested-for-lying-about-ties-to-china":
                 try:
                     r = requests.get(entity['expanded_url'], timeout=10) 
-                except requests.exceptions.Timeout:
+                except requests.exceptions.RequestException:
                     titles.append("[TIMEOUT ERROR]"+"("+entity['expanded_url'].lower()+")")
-                    urls.append(entity['expanded_url'].lower())
-                    d = parse(data[index]['created_at'])
-                    d = d.strftime('%Y/%m/%d')
-                    dates.append(d)
-                except requests.exceptions.ConnectionError:
-                    titles.append("[CONNECTION ERROR]"+"("+entity['expanded_url'].lower()+")")
-                    urls.append(entity['expanded_url'].lower())
-                    d = parse(data[index]['created_at'])
-                    d = d.strftime('%Y/%m/%d')
-                    dates.append(d)
-                except requests.exceptions.TooManyRedirects:
-                    titles.append("[TOO MANY REDIRECT ERROR]"+"("+entity['expanded_url'].lower()+")")
                     urls.append(entity['expanded_url'].lower())
                     d = parse(data[index]['created_at'])
                     d = d.strftime('%Y/%m/%d')
