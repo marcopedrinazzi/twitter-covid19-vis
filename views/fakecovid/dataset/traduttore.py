d5e677559f9898c59bfd1c74c69a926b7e4c2e12
Marco Pedrinazzi <marcopedrinazzi.mp@gmail.com>
2021-05-02 18:24:05 +0200

    view folder

---
+++ views/fakecovid/dataset/traduttore.py
@@ -0,0 +1,49 @@
+##############
+##########
+#######
+##
+#traduttore
+##
+#######
+##########
+##############
+
+import json
+import sys
+import string
+from google_trans_new import google_translator  
+import time
+
+data = []
+with open('fakecovid_result_final.json', 'r') as f:
+    for line in f:
+        data.append(json.loads(line))
+
+f.close()
+
+
+index=0
+translator = google_translator()  
+for element in data:
+    if data[index]['lang']=="en":
+        print(str(index)+" già inglese")
+    else:
+        translated  = translator.translate(data[index]['full_text'],lang_tgt='en')  
+        data[index]['full_text'] = translated
+        time.sleep(1)
+        print(str(index)+" indice" + data[index]['full_text'])
+        for entity in data[index]['entities']['hashtags']:
+            translated = translator.translate(entity['text'],lang_tgt='en')#lang_tgt è l'alt
+            entity['text']=translated
+            time.sleep(1)
+            print(str(index)+" indice" + entity['text'])
+    index=index+1
+
+
+with open('fakecovid_result_final_translated_full.json', 'a') as f_w:
+    for line_w in data:
+        print("sto stampando")
+        json.dump(line_w, f_w)
+        f_w.write('\n')
+
+f.close()
